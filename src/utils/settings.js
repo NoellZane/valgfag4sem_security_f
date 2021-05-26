8cd6aa29ef9a2e1cf8f804074812f15e5baa1207
Selina <soendengaard@hotmail.com>
2021-05-26 21:45:33 +0200

    small modifies

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -2,7 +2,7 @@
 //Remember to import it!
 
 export const localURL = "http://localhost:8080/security";
-export const onlineURL = "https://swes.me/security/"; 
-//export const userURL = "https://swes.me/security/api/info/";
-export const userURL = "http://localhost:8080/security/api/info/";
+export const onlineURL = "https://swes.me/security"; 
+export const userURL = "https://swes.me/security/api/info/";
+//export const userURL = "http://localhost:8080/security/api/info/";
 export const postURL = "http://localhost:8080/security/api/post/";
