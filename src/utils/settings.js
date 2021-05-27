1e88db5e839760f71da9d17be8a2d29b4b9b5137
Mia Simone <cph-md291@cphbusiness.dk>
2021-05-27 08:29:42 +0200

    nothing

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -2,7 +2,8 @@
 //Remember to import it!
 
 export const localURL = "http://localhost:8080/security";
-export const onlineURL = "https://swes.me/security/"; 
-//export const userURL = "https://swes.me/security/api/info/";
+export const onlineURL = "https://swes.me/security"; 
+//export const userURL = onlineURL + "api/info/";
+//export const postURL = onlineURL + "api/post/";
 export const userURL = "http://localhost:8080/security/api/info/";
 export const postURL = "http://localhost:8080/security/api/post/";
