a8afc8344ba62d3f44a273e31de1a9335f9dd271
NoellZane <cph-ng67@cphbusiness.dk>
2021-05-31 00:08:58 +0200

    Idk if I changed anything pushing to be safe cause it works now

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -2,8 +2,10 @@
 //Remember to import it!
 
 export const localURL = "http://localhost:8080/security";
-export const onlineURL = "https://swes.me/security/"; 
-//export const userURL = "https://swes.me/security/api/info/";
-export const userURL = "http://localhost:8080/security/api/info/";
-export const postURL = "http://localhost:8080/security/api/post/";
- export const avatarURL = "http://localhost:8080/security/api/avatar/";
+export const onlineURL = "https://swes.me/security"; 
+export const userURL = "https://swes.me/security/api/info/";
+//export const userURL = "http://localhost:8080/security/api/info/";
+//export const postURL = "http://localhost:8080/security/api/post/";
+export const postURL = "https://swes.me/security/api/post/";
+ //export const avatarURL = "http://localhost:8080/security/api/avatar/";
+ export const avatarURL = "https://swes.me/security/api/avatar/";
